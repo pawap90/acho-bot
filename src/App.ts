35db90c51943fa639e84ba1cf8c6a88c42a7cfa1
paulasantamaria <m_paula_santamaria@live.com>
2021-09-30 20:55:51 -0300

    Server initialization

--- src/App.ts
+++ src/App.ts
@@ -1,26 +1,85 @@
 import * as tmi from 'tmi.js';
 import * as dotenv from 'dotenv';
+import * as passport from 'passport';
+import * as FlatCache from 'flat-cache';
+import TwitchService from './services/twitch.service';
+import ServerInit from './server-init';
+
+const cache = FlatCache.load('acho-bot-cache');
 
 dotenv.config();
 
-const client = new tmi.Client({
-	options: { debug: process.env.TMI_DEBUG ? true : false, messagesLogLevel: process.env.TMI_LOGLEVEL },
-	connection: {
-		reconnect: true,
-		secure: false
-	},
-	identity: {
-		username: process.env.TWITCH_BOT_USERNAME,
-		password: process.env.TWITCH_BOT_OAUTHTOKEN
-	},
-	channels: `${process.env.TWITCH_CHANNELS}`.split(',')
+const app = ServerInit.init();
+
+const twitchService = new TwitchService({
+	clientId: process.env.TWITCH_BOT_CLIENTID!,
+	clientSecret: process.env.TWITCH_BOT_CLIENTSECRET!,
+	redirectUri: process.env.TWITCH_BOT_REDIRECTURI!
 });
 
-client.connect().catch(console.error);
+app.get('/twitch/authorize', passport.authenticate('twitch', { scope: 'chat:read chat:edit' }));
 
-client.on('message', (channel, tags, message, self) => {
-	if (self) return;
-	if (message.toLowerCase() === '!hello') {
-		client.say(channel, `@${tags.username}, heya!`);
+app.get('/twitch/token', passport.authenticate('twitch', { successRedirect: '/', failureRedirect: '/' }));
+
+app.get('/', async (req, res) => {
+
+	let accessToken = cache.getKey('twitch-access-token');
+
+	try {
+		const validationResult = await twitchService.validateAccessToken(accessToken);
+
+		if (!validationResult.isValid) {
+
+			// TODO: Refresh and validate again.
+			// const refreshToken = cache.getKey('twitch-refresh-token');
+			// const tokenInfo = await twitchService.refreshToken(refreshToken)
+
+			res.json({ message: 'User authorization needed' }).status(401);
+		}
+		else {
+			res.json({ message: 'Ready to join chat' });
+		}
+
 	}
+	catch (err) {
+		res.json({ message: 'Error validating token' }).status(500);
+	}
+
+	// if (myCache.get<string>('twitch-access-token')) {
+	// 	startTmiClient();
+	// 	res.send('<html><head><title>AchoBot</title></head>Connected</html>');
+	// } else {
+	// 	res.send('<html><head><title>AchoBot</title></head><a href="/twitch/authorize">Connect with Twitch account</a></html>');
+	// }
 });
+
+app.listen(5000, function () {
+	console.info(`AchoBot running on port ${process.env.PORT}`)
+});
+
+function startTmiClient() {
+	const client = new tmi.Client({
+		options: { debug: process.env.TMI_DEBUG ? true : false, messagesLogLevel: process.env.TMI_LOGLEVEL },
+		connection: {
+			reconnect: true,
+			secure: true
+		},
+		identity: {
+			username: process.env.TWITCH_BOT_USERNAME,
+			password: 'oauth:lsa9ngwhjnicqy886r0dcma8l66rfe' //process.env.TWITCH_BOT_OAUTHTOKEN
+		},
+		channels: `${process.env.TWITCH_CHANNELS}`.split(',')
+	});
+
+	client.connect().catch((err) => {
+		console.log(err);
+	});
+
+	client.on('message', (channel, tags, message, self) => {
+		if (self) return;
+		if (message.toLowerCase() === '!hello') {
+			client.say(channel, `@${tags.username}, heya!`);
+		}
+	});
+
+}
---
+++ src/server-init.ts
@@ -0,0 +1,62 @@
+import * as express from 'express';
+import * as session from 'express-session';
+import * as passport from 'passport';
+import * as OAuth2Strategy from 'passport-oauth2';
+import * as FlatCache from 'flat-cache';
+import TwitchService from './services/twitch.service';
+
+export default class ServerInit {
+    static init(): express.Express {
+
+        const cache = FlatCache.load('acho-bot-cache');
+
+        const app = express();
+        app.use(session({ secret: process.env.SESSION_SECRET!, resave: false, saveUninitialized: false }));
+        app.use(passport.initialize());
+        app.use(passport.session());
+
+        const twitchService = new TwitchService({
+            clientId: process.env.TWITCH_BOT_CLIENTID!,
+            clientSecret: process.env.TWITCH_BOT_CLIENTSECRET!,
+            redirectUri: process.env.TWITCH_BOT_REDIRECTURI!
+        });
+
+        OAuth2Strategy.prototype.userProfile = function (accessToken, done) {
+            twitchService.getUserProfile(accessToken)
+                .then(res => {
+                    done(null, res);
+                })
+                .catch(err => done(err));
+        }
+
+        passport.serializeUser(function (user, done) {
+            done(null, user);
+        });
+
+        passport.deserializeUser((id, done) => {
+            done(null, id as Express.User);
+        });
+
+        passport.use('twitch', new OAuth2Strategy({
+            authorizationURL: TwitchService.endpoints.OAUTH_AUTHORIZE,
+            tokenURL: TwitchService.endpoints.OAUTH_TOKEN,
+            clientID: twitchService.config.clientId,
+            clientSecret: twitchService.config.clientSecret,
+            callbackURL: twitchService.config.redirectUri,
+            state: true
+        },
+            function (accessToken: string, refreshToken: string, profile: any, done: OAuth2Strategy.VerifyCallback) {
+                profile.accessToken = accessToken;
+                profile.refreshToken = refreshToken;
+
+                cache.setKey('twitch-access-token', accessToken);
+                cache.setKey('twitch-refresh-token', refreshToken);
+                cache.save();
+
+                done(null, profile);
+            }
+        ));
+
+        return app;
+    }
+}
