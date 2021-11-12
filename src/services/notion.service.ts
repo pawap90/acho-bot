2d9936bb78791742dd8d69af6377e098d5339d03
paulasantamaria <m_paula_santamaria@live.com>
2021-11-12 09:46:43 -0300

    Add notion service to retrieve database pages

--- src/services/notion.service.ts
+++ src/services/notion.service.ts
@@ -1,31 +1,31 @@
-import { FetchBuilder, FetchBuilderError } from '../utils/fetch.builder';
+import { FetchBuilder } from "../utils/fetch.builder";
 
 type NotionConfig = {
     version: string,
     apiKey: string
 }
 
+type NotionDatabaseSimpleRecord = { [key: string]: string }
+
 export class NotionService {
 
     private readonly config: NotionConfig;
 
-
-    private readonly endpoints = {
+    private endpoints = {
         GET_DATABASE: 'https://api.notion.com/v1/databases/{0}/query'
     }
 
     constructor(config?: NotionConfig) {
-
         this.config = config ?? {
             version: process.env.NOTION_VERSION!,
             apiKey: process.env.NOTION_APIKEY!
-        }
+        };
     }
-
 
     async getDatabasePage<TRecord>(databaseId: string, pageSize: number = 100, cursor?: string): Promise<TRecord[]> {
         const endpoint = this.endpoints.GET_DATABASE.replace('{0}', databaseId);
-        const database = await FetchBuilder
+
+        const databaseRecords = await FetchBuilder
             .post<any>(endpoint)
             .setHeaders({
                 'Authorization': 'Bearer ' + this.config.apiKey,
@@ -37,16 +37,16 @@ export class NotionService {
             })
             .execute();
 
-        const records: any[] = this.mapRecords(database.body);
+        const records: any[] = this.mapDatabaseRecords(databaseRecords.body);
 
         return records;
     }
 
-    private mapRecords(database: any): NotionDatabaseSimpleRecord[] {
-        const records: NotionDatabaseSimpleRecord[] = database.results.map((r: { properties: { [x: string]: any; }; }) => {
-
-            const record: NotionDatabaseSimpleRecord = {}
+    private mapDatabaseRecords(databaseRecords: any): NotionDatabaseSimpleRecord[] {
+        const records: NotionDatabaseSimpleRecord[] = databaseRecords.results.map((r: { properties: { [x: string]: any; }; }) => {
+            const record: NotionDatabaseSimpleRecord = {};
             let propertyNames = Object.keys(r.properties);
+
             for (const propertyKey in propertyNames) {
                 const propertyName = propertyNames[propertyKey];
                 const propertyValue = r.properties[propertyName];
@@ -54,12 +54,10 @@ export class NotionService {
                 if (propertyValue[propertyValue.type] && propertyValue[propertyValue.type][0])
                     record[propertyName] = propertyValue[propertyValue.type][0]?.plain_text;
             }
+            
             return record;
         });
-        
+
         return records;
     }
 }
-
-type NotionDatabaseSimpleRecord = { [key: string]: string }
-
