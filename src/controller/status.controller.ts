2a864bd94367756df76dc8f7675e09fdae5ffee2
paulasantamaria <m_paula_santamaria@live.com>
2021-11-12 09:41:30 -0300

    Add bot status endpoint

---
+++ src/controller/status.controller.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import { TwitchAuthorizationRequiredError, TwitchService } from "../services/twitch.service";
+
+export default class StatusController {
+
+    static routes(): Router {
+        const router = Router();
+
+        const twitchService = new TwitchService();
+
+        router.get('/', async (req, res, next) => {
+            try {
+                await twitchService.getValidAccessToken();
+                res.json({ message: 'Ready to chat!' })
+            }
+            catch (err) {
+                if (err instanceof TwitchAuthorizationRequiredError)
+                    res.json({ message: 'AchoBot requires authorization with Twitch' }).status(401);
+                
+                next(err);
+            }
+        });
+
+        return router;
+    }
+}
