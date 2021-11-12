3ce3f45686728ed2c188b651ea19a50e3e90201f
paulasantamaria <m_paula_santamaria@live.com>
2021-11-12 09:36:35 -0300

    Twitch authorization improvements

--- src/services/twitch.service.ts
+++ src/services/twitch.service.ts
@@ -1,5 +1,6 @@
-import { FetchBuilder, FetchBuilderError } from '../utils/fetch.builder';
-import CacheService from './cache.service';
+import { FetchBuilder, FetchBuilderError } from "../utils/fetch.builder";
+import CacheService from "./cache.service";
+
 
 type TwitchConfig = {
     clientId: string,
@@ -18,15 +19,16 @@ export class TwitchService {
         GET_USERS: 'https://api.twitch.tv/helix/users'
     };
 
-    constructor(config?: TwitchConfig) {
-        this.config = config ?? {
+    constructor() {
+        this.config = {
             clientId: process.env.TWITCH_BOT_CLIENTID!,
             clientSecret: process.env.TWITCH_BOT_CLIENTSECRET!,
             redirectUri: process.env.TWITCH_BOT_REDIRECTURI!
-        };
+        }
     }
 
     async getUserProfile(accessToken: string): Promise<TwitchUser> {
+
         const result = await FetchBuilder
             .get<TwitchUser>(TwitchService.endpoints.GET_USERS)
             .setHeaders({
@@ -40,25 +42,29 @@ export class TwitchService {
     }
 
     async getValidAccessToken(): Promise<string> {
+        // Try to get the access token from cache
         const accessToken = CacheService.getAccessToken();
+        const refreshToken = CacheService.getRefreshToken();
 
-        if (accessToken && accessToken.length > 0) {
-            const validationResult = await this.validateAccessToken(accessToken);
+        if (refreshToken && refreshToken.length > 0 && accessToken && accessToken.length > 0) {
+            // If there's a token, check if it's valid.
+            const tokenValidationResult = await this.validateAccessToken(accessToken);
 
-            if (validationResult.isValid)
+            if (tokenValidationResult.isValid)
                 return accessToken;
 
-            const refreshToken = CacheService.getRefreshToken();
-            const tokens = await this.generateNewAccessToken(refreshToken);
+            // If it's not valid, refresh
+            const tokens = await this.generateNewTokens(refreshToken);
 
             CacheService.storeAccessToken(tokens.access_token);
             CacheService.storeRefreshToken(tokens.refresh_token);
         }
 
-        throw new TwitchAuthorizationRequiredError();
+        // If there's no access token, throw error
+        throw new TwitchAuthorizationRequiredError("No token in cache");
     }
 
-    async validateAccessToken(accessToken: string): Promise<TwitchTokenValidation> {
+    async validateAccessToken(accessToken: string): Promise<TwitchTokenValidationResult> {
         try {
             const result = await FetchBuilder
                 .get<TwitchTokenInformation>(TwitchService.endpoints.OAUTH_VALIDATE)
@@ -67,31 +73,28 @@ export class TwitchService {
                 })
                 .execute();
 
-            const validationResult = {
+            return {
                 isValid: true,
                 tokenInformation: result.body
-            } as TwitchTokenValidation;
-
-            return validationResult;
+            };
         }
         catch (err) {
-            if (err instanceof FetchBuilderError && (err as FetchBuilderError).status == 401)
-                return { isValid: false };
+            if (err instanceof FetchBuilderError && (err as FetchBuilderError).status === 401)
+                return { isValid: false }
 
             throw err;
         }
     }
 
-    async generateNewAccessToken(refreshToken: string): Promise<TwitchToken> {
+    async generateNewTokens(refreshToken: string): Promise<TwitchToken> {
 
         let endpoint = TwitchService.endpoints.OAUTH_TOKEN;
         endpoint += `?client_id=${this.config.clientId}`;
         endpoint += `&client_secret=${this.config.clientSecret}`;
         endpoint += `&refresh_token=${refreshToken}`;
-        endpoint += '&grant_type=refresh_token';
+        endpoint += `&grant_type=refresh_token`;
 
-        const result = await FetchBuilder
-            .post<TwitchToken>(endpoint)
+        const result = await FetchBuilder.post<TwitchToken>(endpoint)
             .execute();
 
         return result.body;
@@ -100,20 +103,23 @@ export class TwitchService {
 
 export class TwitchAuthorizationRequiredError extends Error {
     constructor(message?: string) {
-        super(message ?? 'Authorization required');
+        super(message ?? 'Twitch Authorization required');
 
         Object.setPrototypeOf(this, TwitchAuthorizationRequiredError.prototype);
     }
 }
 
-type TwitchUser = {
-    broadcaster_type: string,
-    description: string,
-    display_name: string,
+type TwitchTokenValidationResult = {
+    isValid: boolean,
+    tokenInformation?: TwitchTokenInformation
+}
+
+type TwitchTokenInformation = {
+    client_id: string,
     login: string,
-    profile_image_url: string,
-    type: string,
-    created_at: string,
+    user_id: string,
+    expires_in: number,
+    scope: string[]
 }
 
 type TwitchToken = {
@@ -124,16 +130,12 @@ type TwitchToken = {
     token_type: string
 }
 
-type TwitchTokenInformation = {
-    isValid: boolean,
-    client_id: string,
+type TwitchUser = {
+    broadcaster_type: string,
+    description: string,
+    display_name: string,
     login: string,
-    user_id: string,
-    expires_in: number,
-    scope: string[]
-}
-
-type TwitchTokenValidation = {
-    isValid: boolean,
-    tokenInformation?: TwitchTokenInformation
+    profile_image_url: string,
+    type: string;
+    created_at: string
 }
