b35f7337dae503835126cab61afcf878b8d1a98f
paulasantamaria <m_paula_santamaria@live.com>
2021-11-12 09:49:45 -0300

    Add status views

---
+++ src/controller/home.controller.ts
@@ -0,0 +1,31 @@
+import { Router } from "express";
+import { TwitchAuthorizationRequiredError, TwitchService } from "../services/twitch.service";
+
+export default class HomeController {
+
+    static routes(): Router {
+        const router = Router();
+
+        const twitchService = new TwitchService();
+
+        router.get('/', async (req, res, next) => {
+            try {
+                await twitchService.getValidAccessToken();
+                res.render('status.pug', { status: 200 });
+            }
+            catch (err) {
+                if (err instanceof TwitchAuthorizationRequiredError)
+                    res.render('status.pug', { status: 401 });
+                
+                res.render('status.pug', { status: 500 });
+                next(err);
+            }
+        });
+
+        router.get('/commands', async (req, res, next) => {
+            res.render('commands.pug', { commands: ['!hello', '!bye'] })
+        });
+
+        return router;
+    }
+}
