dbd14e06a16d4706a630ec3599eafb491a754463
paulasantamaria <m_paula_santamaria@live.com>
2021-12-16 22:38:58 -0300

    Map different types of columns to string

--- src/services/notion.service.ts
+++ src/services/notion.service.ts
@@ -51,13 +51,28 @@ export class NotionService {
                 const propertyName = propertyNames[propertyKey];
                 const propertyValue = r.properties[propertyName];
 
-                if (propertyValue[propertyValue.type] && propertyValue[propertyValue.type][0])
-                    record[propertyName] = propertyValue[propertyValue.type][0]?.plain_text;
+                if (propertyValue[propertyValue.type]) {
+                    const mapper = this.mappers[propertyValue.type];
+                    if (mapper)
+                        record[propertyName] = mapper(propertyValue);
+                }
             }
-            
+
             return record;
         });
 
         return records;
     }
+
+    private mappers: NotionColumMappers = {
+        'rich_text': (propertyValue) => propertyValue[propertyValue.type][0]?.plain_text,
+        'multi_select': (propertyValue) => propertyValue[propertyValue.type].map((item: any) => item.name as string).join(','),
+        'select': (propertyValue) => propertyValue[propertyValue.type]?.name as string
+    }
 }
+
+type NotionColumMappers = {
+    [key: string]: NotionColumnMapperFunction
+}
+
+type NotionColumnMapperFunction = (propertyValue: any) => string;
