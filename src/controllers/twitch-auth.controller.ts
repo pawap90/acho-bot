01d1feadd718ac5ce199b783cb52e70151889f38
paulasantamaria <m_paula_santamaria@live.com>
2022-02-04 10:05:03 -0300

    Simplify authorizaiton flow

--- src/controllers/twitch-auth.controller.ts
+++ src/controllers/twitch-auth.controller.ts
@@ -8,9 +8,9 @@ export default class TwitchAuthController {
 
         router.get('/', passport.authenticate('twitch', { scope: 'chat:read chat:edit channel:read:subscriptions' }));
 
-        router.get('/token', passport.authenticate('twitch', { successRedirect: '/api/status', failureRedirect: '/api/twitch/auth/failed' }));
+        router.get('/token', passport.authenticate('twitch', { successRedirect: '/', failureRedirect: '/api/twitch/auth/failed' }));
 
-        router.get('failed', (req, res) => {
+        router.get('/failed', (req, res) => {
             res.json({ message: 'Authorization with Twitch failed' }).status(401);
         });
 
