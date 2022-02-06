13bd5a9c97cef2af9dd81fc0f07cb7b5d89d9709
paulasantamaria <m_paula_santamaria@live.com>
2022-02-06 13:57:47 -0300

    Add refresh command

--- src/commands/loaders/builtin-commands.loader.ts
+++ src/commands/loaders/builtin-commands.loader.ts
@@ -1,12 +1,14 @@
 import { HelpCommand } from '../help.command';
 import { TmiCommandDictionary, ICommandLoader } from '../itmi.command';
+import { RefreshCommand } from '../refresh.command';
 
 
 export class BuiltInCommandsLoader implements ICommandLoader {
     async load(): Promise<TmiCommandDictionary> {
 
         return {
-            '!help': new HelpCommand()
+            '!help': new HelpCommand(),
+            '!refresh': new RefreshCommand()
         };
     }
 }
